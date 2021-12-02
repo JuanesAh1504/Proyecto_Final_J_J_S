51f3abe14ef2b95e0e0b48c0fedc8ca330db6777
Juanes Álvarez <juanestebanalvarezholguin@gmail.com>
2021-12-02 07:39:41 -0500

    .

---
+++ libs/js/idramdon.js
@@ -0,0 +1,9 @@
+function elNombreDeLaFuncion() { 
+    
+    document.getElementById("Remplazable").value = names;
+   
+}
+function theSelector() {
+  var names = pickRandom(['Mister Turtle','Ultra NPC','Generic    |      unicorn','Auto egg','MiniBoby','Ultra nova','Moon travel'])
+   window.addEventListener('load',elNombreDeLaFuncion)
+}
