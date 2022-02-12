b42eb860ffe729d67b141de388fd9f18301f294d
Juanes Álvarez <juanestebanalvarezholguin@gmail.com>
2022-02-11 22:46:06 -0500

    Login actualizado

--- index.php
+++ index.php
@@ -4,26 +4,56 @@
   require_once('includes/load.php');
   if($session->isUserLoggedIn(true)) { redirect('home.php', false);}
 ?>
-<?php include_once('layouts/header.php'); ?>
 
-<div class="login-page">
-    <div class="text-center">
-       <h1>Bienvenido </h1>
-       <p>Iniciar sesión </p>
-     </div>
-     <?php echo display_msg($msg); ?>
-      <form method="post" action="auth.php" class="clearfix">
-        <div class="form-group">
-              <label for="username" class="control-label">Usario</label>
-              <input type="name" class="form-control" name="username" placeholder="Usario">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://preview.colorlib.com/theme/bootstrap/login-form-20/css/A.style.css.pagespeed.cf.eQk9-CoeFP.css">
+<script>(function(w,d){!function(a,e,t,r,z){a.zarazData=a.zarazData||{},a.zarazData.executed=[],a.zarazData.tracks=[],a.zaraz={deferred:[]};var s=e.getElementsByTagName("title")[0];a.zarazData.c=e.cookie,s&&(a.zarazData.t=e.getElementsByTagName("title")[0].text),a.zarazData.w=a.screen.width,a.zarazData.h=a.screen.height,a.zarazData.j=a.innerHeight,a.zarazData.e=a.innerWidth,a.zarazData.l=a.location.href,a.zarazData.r=e.referrer,a.zarazData.k=a.screen.colorDepth,a.zarazData.n=e.characterSet,a.zarazData.o=(new Date).getTimezoneOffset(),//
+a.dataLayer=a.dataLayer||[],a.zaraz.track=(e,t)=>{for(key in a.zarazData.tracks.push(e),t)a.zarazData["z_"+key]=t[key]},a.zaraz._preSet=[],a.zaraz.set=(e,t,r)=>{a.zarazData["z_"+e]=t,a.zaraz._preSet.push([e,t,r])},a.dataLayer.push({"zaraz.start":(new Date).getTime()}),a.addEventListener("DOMContentLoaded",(()=>{var t=e.getElementsByTagName(r)[0],z=e.createElement(r);z.defer=!0,z.src="/cdn-cgi/zaraz/s.js?z="+btoa(encodeURIComponent(JSON.stringify(a.zarazData))),t.parentNode.insertBefore(z,t)}))}(w,d,0,"script");})(window,document);</script></head>
+<body class="img js-fullheight" style="background-image:url(https://static1.educaedu-colombia.com/adjuntos/12/00/36/corporaci-n-universitaria-minuto-de-dios---uniminuto-sede-bello--003634_large.jpg)">
+  <section class="ftco-section">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-md-6 text-center mb-5">
+          <h2 class="heading-section" style="font-weight:bold;text-shadow: -2px -2px 1px #000, 2px 2px 1px #000, -2px 2px 1px #000, 2px -2px 1px #000;">BIENVENIDO AL SISTEMA DE CONSULTAS Y PRÉSTAMOS CPA</h2>
         </div>
-        <div class="form-group">
-            <label for="Password" class="control-label">Contraseña</label>
-            <input type="password" name= "password" class="form-control" placeholder="Contraseña">
+      </div>
+      <div class="row justify-content-center">
+        <div class="col-md-6 col-lg-4">
+          <div class="login-wrap p-0">
+            <h3 class="mb-4 text-center" style="font-weight:bold">Universidad Minuto de Dios</h3>
+            <form action="auth.php" method="POST" class="signin-form">
+            <div class="form-group">
+              <input type="text" class="form-control" placeholder="Username" name="username" required>
+            </div>
+            <div class="form-group">
+              <input id="password-field" type="password" class="form-control" placeholder="Password" name="password" required>
+              <span toggle="#password-field" class="fa fa-fw fa-eye field-icon toggle-password"></span>
+            </div>
+            <div class="form-group">
+              <button type="submit" class="form-control btn submit px-3" style="background-color:#138fcb">INGRESAR</button>
+            </div>
+            <div class="form-group d-md-flex">
+              <!-- <div class="w-50">
+                <label class="checkbox-wrap checkbox-primary">Remember Me
+                <input type="checkbox" checked>
+                <span class="checkmark"></span>
+                </label>
+              </div> -->
+              <!-- <div class="w-50 text-md-right">
+                <a href="#" style="color: #fff">Forgot Password</a>
+              </div> -->
+            </div>
+            </form>
+          </div>
         </div>
-        <div class="form-group">
-                <button type="submit" class="btn btn-info  pull-right">Entrar</button>
-        </div>
-    </form>
-</div>
+      </div>
+    </div>
+</section>
+<script src="js/jquery.min.js"></script>
+<script src="js/popper.js+bootstrap.min.js+main.js.pagespeed.jc.9eD6_Mep8S.js"></script><script>eval(mod_pagespeed_T07FyiNNgg);</script>
+<script>eval(mod_pagespeed_zB8NXha7lA);</script>
+<script>eval(mod_pagespeed_xfgCyuItiV);</script>
+<script defer src="https://static.cloudflareinsights.com/beacon.min.js/v652eace1692a40cfa3763df669d7439c1639079717194" integrity="sha512-Gi7xpJR8tSkrpF7aordPZQlW2DLtzUlZcumS8dMQjwDHEnw9I7ZLyiOj/6tZStRBGtGgN6ceN6cMH8z7etPGlw==" data-cf-beacon='{"rayId":"6d750ea16d1367e6","token":"cd0b4b3a733644fc843ef0b185f98241","version":"2021.12.0","si":100}' crossorigin="anonymous"></script>
 <?php include_once('layouts/footer.php'); ?>
